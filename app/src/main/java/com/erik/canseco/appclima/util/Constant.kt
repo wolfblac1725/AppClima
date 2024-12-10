d57ece36255ec773dee8674b9a60c6ba43ab07cb
erikcanseco <wolfblac@gmail.com>
2024-12-09 22:56:05 -0600

    first commit

--- app/src/main/java/com/erik/canseco/appclima/util/Constant.kt
+++ app/src/main/java/com/erik/canseco/appclima/util/Constant.kt
@@ -2,7 +2,7 @@ package com.erik.canseco.appclima.util
 
 object Constant {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "ddd5f1d8b23350a7eb3bce0b4d875e39"
+    const val API_KEY = "ddd5f1d8b24450d7eb3bere0b4d875e39"
     const val UNITS = "metric"
     const val LANG = "es"
     const val CNT = "15"
